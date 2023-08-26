aeca001794b7002c54a7455c5b63086c95cc8b2e
Brandon Slade <brandonyoyoslade@gmail.com>
2023-08-26 12:14:26 -0600

    feat(rustlings): complete primitive_types exercises

--- rustlings/exercises/primitive_types/primitive_types1.rs
+++ rustlings/exercises/primitive_types/primitive_types1.rs
@@ -6,17 +6,13 @@
 // Execute `rustlings hint primitive_types1` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    // Booleans (`bool`)
-
     let is_morning = true;
     if is_morning {
         println!("Good morning!");
     }
 
-    let // Finish the rest of this line like the example! Or make it be false!
+    let is_evening = false;
     if is_evening {
         println!("Good evening!");
     }
--- rustlings/exercises/primitive_types/primitive_types2.rs
+++ rustlings/exercises/primitive_types/primitive_types2.rs
@@ -6,11 +6,7 @@
 // Execute `rustlings hint primitive_types2` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    // Characters (`char`)
-
     // Note the _single_ quotes, these are different from the double quotes
     // you've been seeing around.
     let my_first_initial = 'C';
@@ -22,7 +18,7 @@ fn main() {
         println!("Neither alphabetic nor numeric!");
     }
 
-    let // Finish this line like the example! What's your favorite character?
+    let your_character = 'z'; // Finish this line like the example! What's your favorite character?
     // Try a letter, try a number, try a special character, try a character
     // from a different language than your own, try an emoji!
     if your_character.is_alphabetic() {

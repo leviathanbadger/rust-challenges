d9b3be6c3828b1843b48150e776407a1b029c56d
Brandon Slade <brandonyoyoslade@gmail.com>
2024-06-16 14:44:18 -0600

    feat(adventofcode): add day 9 part 2

--- adventofcode/day9/src/main.rs
+++ adventofcode/day9/src/main.rs
@@ -46,10 +46,47 @@ fn extrapolate_next(history: &Vec<i64>) -> i64 {
     first[first.len() - 1]
 }
 
+fn extrapolate_previous(history: &Vec<i64>) -> i64 {
+    let mut differences = vec![history.clone()];
+
+    loop {
+        let previous = &differences[differences.len() - 1];
+        if previous.iter().all(|n| *n == 0) {
+            break;
+        }
+        let next = previous
+            .iter()
+            .tuple_windows()
+            .map(|(a, b)| *b - *a)
+            .collect::<Vec<i64>>();
+        differences.push(next);
+    }
+
+    let last_idx = differences.len() - 1;
+    differences[last_idx].push(0);
+
+    for q in (0..last_idx).rev() {
+        let after = &differences[q + 1];
+        let diff = after[after.len() - 1];
+        let current = &mut differences[q];
+        current.push(current[0] - diff);
+    }
+
+    let first = &differences[0];
+    first[first.len() - 1]
+}
+
 fn day9part1(histories: &Vec<Vec<i64>>) -> i64 {
     histories
         .iter()
         .map(|history| extrapolate_next(history))
+        .sum()
+}
+
+fn day9part2(histories: &Vec<Vec<i64>>) -> i64 {
+    histories
+        .iter()
+        .map(|history| extrapolate_previous(history))
         .sum()
 }
 
@@ -59,6 +96,7 @@ fn main() -> Result<()> {
     let histories = parse_histories(&input)?;
 
     println!("Day 9 part 1 answer: {}", day9part1(&histories));
+    println!("Day 9 part 2 answer: {}", day9part2(&histories));
 
     Ok(())
 }
@@ -75,6 +113,18 @@ mod tests {
         let histories = parse_histories(&INPUT.to_owned())?;
 
         assert_eq!(day9part1(&histories), 114);
+
+        Ok(())
+    }
+
+    #[test]
+    fn day9part2_returns_correct_value() -> Result<()> {
+        const INPUT: &str = "0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45";
+        let histories = parse_histories(&INPUT.to_owned())?;
+
+        assert_eq!(day9part2(&histories), 2);
 
         Ok(())
     }

e7c9b77a46bcae72b7bba0fd1d1d198c2936e88d
Brandon Slade <brandonyoyoslade@gmail.com>
2024-06-16 21:16:50 -0600

    feat(adventofcode): add day 11 part 2

--- adventofcode/day11/src/main.rs
+++ adventofcode/day11/src/main.rs
@@ -3,59 +3,70 @@ use std::{collections::HashSet, fs};
 use anyhow::*;
 use itertools::Itertools;
 
-fn parse_galaxy_map(input: &String) -> Vec<(usize, usize)> {
+fn parse_galaxy_map(input: &String, expand_amt: u64) -> Vec<(u64, u64)> {
     let galaxies_before_expansion = input.lines()
         .enumerate()
         .flat_map(|(y, line)| line.as_bytes()
             .iter()
             .enumerate()
             .filter(|(_, chr)| **chr == b'#')
-            .map(move |(x, _)| (x, y)))
-        .collect::<Vec<(usize, usize)>>();
+            .map(move |(x, _)| (x as u64, y as u64)))
+        .collect::<Vec<(u64, u64)>>();
 
-    let gp_horiz = HashSet::<usize>::from_iter(galaxies_before_expansion
+    let gp_horiz = HashSet::<u64>::from_iter(galaxies_before_expansion
         .iter()
         .map(|(x, _)| *x)
     );
 
-    let gp_vert = HashSet::<usize>::from_iter(galaxies_before_expansion
+    let gp_vert = HashSet::<u64>::from_iter(galaxies_before_expansion
         .iter()
         .map(|(_, y)| *y)
     );
 
     let mut expand_horiz = (0..(*gp_horiz.iter().max().unwrap_or(&0)))
         .filter(|x| !gp_horiz.contains(x))
-        .collect::<Vec<usize>>();
+        .collect::<Vec<u64>>();
     expand_horiz.sort_unstable();
     let mut expand_vert = (0..(*gp_vert.iter().max().unwrap_or(&0)))
         .filter(|y| !gp_vert.contains(y))
-        .collect::<Vec<usize>>();
+        .collect::<Vec<u64>>();
     expand_vert.sort_unstable();
 
     galaxies_before_expansion
         .iter()
-        .map(|(x, y)| (*x + expand_horiz.iter().filter(|tx| **tx < *x).count(), *y + expand_vert.iter().filter(|ty| **ty < *y).count()))
-        .collect::<Vec<(usize, usize)>>()
+        .map(|(x, y)| (*x + (expand_horiz.iter().filter(|tx| **tx < *x).count() as u64) * expand_amt, *y + (expand_vert.iter().filter(|ty| **ty < *y).count() as u64) * expand_amt))
+        .collect::<Vec<(u64, u64)>>()
 }
 
-fn day11part1(map: &Vec<(usize, usize)>) -> usize {
+fn absdiff(a: u64, b: u64) -> u64 {
+    if a > b {
+        a - b
+    }
+    else {
+        b - a
+    }
+}
+
+fn day11(input: &String, expand_amt: u64) -> u64 {
+    let map = parse_galaxy_map(&input, expand_amt);
+
     let pairs = map
         .iter()
         .tuple_combinations()
-        .collect::<Vec<(&(usize, usize), &(usize, usize))>>();
+        .collect::<Vec<(&(u64, u64), &(u64, u64))>>();
 
     pairs
         .iter()
-        .map(|(a, b)| (i32::abs((b.0 as i32) - (a.0 as i32)) + i32::abs((b.1 as i32) - (a.1 as i32))) as usize)
+        .map(|(a, b)| absdiff(a.0, b.0) + absdiff(a.1, b.1))
         .sum()
 }
 
 fn main() -> Result<()> {
     let input = fs::read_to_string("input.txt")
         .expect("Could not read input.txt");
-    let map = parse_galaxy_map(&input);
 
-    println!("Day 11 part 1 answer: {}", day11part1(&map));
+    println!("Day 11 part 1 answer: {}", day11(&input, 1));
+    println!("Day 11 part 2 answer: {}", day11(&input, 1000000 - 1));
 
     Ok(())
 }
@@ -65,7 +76,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn day11part1_returns_correct_value() -> Result<()> {
+    fn day11_returns_correct_value() {
         const INPUT: &str = "...#......
 .......#..
 #.........
@@ -76,10 +87,39 @@ mod tests {
 ..........
 .......#..
 #...#.....";
-        let map = parse_galaxy_map(&INPUT.to_owned());
 
-        assert_eq!(day11part1(&map), 374);
+        assert_eq!(day11(&INPUT.to_owned(), 1), 374);
+    }
 
-        Ok(())
+    #[test]
+    fn day11_returns_correct_value_2() {
+        const INPUT: &str = "...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....";
+
+        assert_eq!(day11(&INPUT.to_owned(), 9), 1030);
+    }
+
+    #[test]
+    fn day11_returns_correct_value_3() {
+        const INPUT: &str = "...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....";
+
+        assert_eq!(day11(&INPUT.to_owned(), 99), 8410);
     }
 }

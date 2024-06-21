c1f756fff484afc1b50f154b5c76d1af7890d6cc
Brandon Slade <brandonyoyoslade@gmail.com>
2024-06-21 09:17:29 -0600

    test(calc-eval): add unit tests for try_parse_expression

--- calc-eval/src/calculator/syntax/expression_syntax.rs
+++ calc-eval/src/calculator/syntax/expression_syntax.rs
@@ -32,3 +32,53 @@ pub trait ExpressionSyntax: Syntax {
 pub fn try_parse_expression(tokens: &Vec<Token>, pos: &mut usize) -> Option<Box<dyn ExpressionSyntax>> {
     AdditiveExpressionSyntax::try_parse_expression(tokens, pos)
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::calculator::tokenizer::Tokenizer;
+    use super::*;
+
+    #[test]
+    fn try_parse_expression_should_correctly_parse_expressions() {
+        let test_cases: &[(&str, &str)] = &[
+            //Primary
+            ("25", "25"),
+            ("2.5", "2.5"),
+            ("(((1)))", "1"),
+
+            //Unary
+            ("-42", "-42"),
+            ("-(42)", "-42"),
+            ("+-+-+42", "+-+-+42"),
+
+            //Multiplicative
+            ("1*2/3%4", "1 * 2 / 3 % 4"),
+            ("1*(2/(3%4))", "1 * (2 / (3 % 4))"),
+            ("-1*-2", "-1 * -2"),
+            ("-(1*2)", "-(1 * 2)"),
+
+            //Additive
+            ("1+2-3", "1 + 2 - 3"),
+            ("1+(2-3)", "1 + (2 - 3)"),
+            ("-1+-2", "-1 + -2"),
+            ("-(1+2)", "-(1 + 2)"),
+            ("1*2+3*4", "1 * 2 + 3 * 4"),
+            ("1*(2+3)*4", "1 * (2 + 3) * 4"),
+            ("1+2/3-4", "1 + 2 / 3 - 4"),
+            ("(1+2)/(3-4)", "(1 + 2) / (3 - 4)"),
+        ];
+
+        let tokenizer = Tokenizer::new();
+
+        for &(input, expected_output) in test_cases {
+            let tokens = tokenizer.tokenize(input).collect();
+
+            let mut pos = 0;
+            let expr = try_parse_expression(&tokens, &mut pos);
+
+            assert!(expr.is_some());
+            assert_eq!(pos, tokens.len() - 1);
+            assert_eq!(expr.unwrap().to_string().as_str(), expected_output);
+        }
+    }
+}

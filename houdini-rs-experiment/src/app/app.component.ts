f61a1c7e8619f592c9d711a0d533cfdf959ad7b7
Brandon Slade <brandonyoyoslade@gmail.com>
2024-06-22 17:41:18 -0600

    feat(houdini-rs): disable zone.js change detection and display timer on the frontend

--- houdini-rs-experiment/src/app/app.component.ts
+++ houdini-rs-experiment/src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { invoke } from "@tauri-apps/api/tauri";
+import { listen, UnlistenFn, Event } from "@tauri-apps/api/event";
 
 @Component({
     selector: 'app-root',
@@ -10,14 +11,34 @@ import { invoke } from "@tauri-apps/api/tauri";
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
-export class AppComponent {
-    greetingMessage = "";
+export class AppComponent implements OnInit, OnDestroy {
+    greetingMessage = signal('');
+
+    timer = signal(0);
+    unlistenFn: UnlistenFn | null = null;
+
+    updateTimer(timer: number) {
+        this.timer.set(timer);
+    }
+
+    ngOnInit(): void {
+        listen('updateTimer', (event: Event<number>) => this.updateTimer(event.payload)).then(unlistenFn => this.unlistenFn = unlistenFn);
+    }
+    ngOnDestroy(): void {
+        this.unlistenFn?.();
+        this.unlistenFn = null;
+    }
 
     async greet(event: SubmitEvent, name: string): Promise<void> {
         event.preventDefault();
 
         // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-        let text = await invoke<string>("greet", { name });
-        this.greetingMessage = text;
+        try {
+            let text = await invoke<string>("greet", { name });
+            this.greetingMessage.set(text);
+        }
+        catch (err) {
+            this.greetingMessage.set(`${err}`);
+        }
     }
 }
